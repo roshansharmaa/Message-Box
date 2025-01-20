68bb09125700ad15fad5a519b0f9edc0c047789b
Roshan Sharma <122720379+roshansharmaa@users.noreply.github.com>
2025-01-20 21:54:17 +0530

    Data.js

--- src/component/Data.js
+++ src/component/Data.js
@@ -4,13 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCynvWzIIGtAq3QO2zzCXRuRwHY9A2hUxQ",
-  authDomain: "react-crud-68d76.firebaseapp.com",
-  projectId: "react-crud-68d76",
-  storageBucket: "react-crud-68d76.firebasestorage.app",
-  messagingSenderId: "633558304055",
-  appId: "1:633558304055:web:18b2107402ad4a86c846b2",
-  measurementId: "G-GXKWZ00TZK"
+ //Add here your api key
 };
 
 const app = initializeApp(firebaseConfig);
